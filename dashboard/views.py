ca178bab1b39946ace001432c039cf6d8c34dfde
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2026-04-05 11:34:29 +0630

    fixed: timezone primarily set to UTC, now change to ASIA YANGON

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,24 +1,22 @@
-from django.shortcuts import render
-from django.http.request import HttpRequest
-from django.contrib.auth.decorators import login_required
-from students.models import Student, EntranceQR
-from entrances.models import Entrance
 from django.utils import timezone
 from django.db.models import Count
 from django.db.models.functions import ExtractHour
+from students.models import Student, EntranceQR
+from entrances.models import Entrance
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 @login_required
 def dashboard_view(request):
-    current_tz = timezone.get_current_timezone()
-    local_now = timezone.localtime(timezone.now())
-    today = local_now.date()
+    now = timezone.now()
+    today = now.date()
 
     student_count = Student.objects.count()
     active_qrs_count = EntranceQR.objects.filter(is_active=True).count()
     
     hourly_data = (
         Entrance.objects.filter(time__date=today)
-        .annotate(hour=ExtractHour('time', tzinfo=current_tz))
+        .annotate(hour=ExtractHour('time'))
         .values('hour')
         .annotate(count=Count('entrance_id'))
         .order_by('hour')
@@ -29,7 +27,7 @@ def dashboard_view(request):
     chart_data = [traffic_dict.get(h, 0) for h in range(9, 17)]
     
     active_percentage = (active_qrs_count / student_count * 100) if student_count > 0 else 0
-    print(hourly_data)
+    
     return render(request, 'dashboard/index.html', {
         "registered_student": student_count,
         "active_qr_percentage": round(active_percentage, 1),
