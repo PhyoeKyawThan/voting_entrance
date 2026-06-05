0e1ca56efbb39454a87dbfe9a51ce870740e52de
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2026-06-06 03:23:08 +0630

    update: docker compose

--- voting_entrance/urls.py
+++ voting_entrance/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -30,4 +31,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    # Daphne does not auto-serve static files like `runserver`; wire them up explicitly.
+    urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
